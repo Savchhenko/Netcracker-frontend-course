67bf46a80956fab03676dce0b3356e6c08334b7a
Savchenko <svchhh@yandex.ru>
2021-11-09 02:07:53 +0300

    fill form with data from localStorage

--- landing-page/main.js
+++ landing-page/main.js
@@ -1,4 +1,26 @@
 const formElem = document.querySelector(".modal-form");
+const formElems = document.querySelectorAll(".form-elem");
+const nameElem = document.querySelector(".name");
+const surnameElem = document.querySelector(".surname");
+const emailElem = document.querySelector(".email");
+const phoneElem = document.querySelector(".phone");
+const textareaElem = document.querySelector(".modal-form-textarea");
+
+window.onload = function() {
+    nameElem.value = localStorage.getItem("name") || [];
+    surnameElem.value = localStorage.getItem("surname") || [];
+    emailElem.value = localStorage.getItem("email") || [];
+    phoneElem.value = localStorage.getItem("phone") || [];
+    textareaElem.value = localStorage.getItem("message") || [];
+}
+
+function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function formValidation() {
+    console.log("something");
+}
 
 function getDataFromForm(formNode) {
     const { elements } = formNode;
@@ -11,11 +33,13 @@ function getDataFromForm(formNode) {
             return { name, value }
         })
         .filter((item) => !!item.name); //убрали элементы без названия, т.е. кнопку
-    console.log(data);
-    return data;
+        return data;
 }
 
 formElem.addEventListener("submit", (event) => {
     event.preventDefault();
-    getDataFromForm(formElem);
+    formValidation();
+    const data = getDataFromForm(formElem);
+    localStorage.clear();
+    localStorage.setItem("Form", JSON.stringify(data));
 })
