6d55d2ca8a3cc8f46bbeb5e6feb2dde941f8836c
Savchenko <svchhh@yandex.ru>
2021-11-11 23:20:01 +0300

    adds all the functionality

--- task_7/main.js
+++ task_7/main.js
@@ -1,46 +1,70 @@
-const tableCellsElems = [...document.getElementsByTagName("td")],
-    btnAddElem = document.querySelector(".options-btn-add"),
-    btnEditElem = document.querySelector(".options-btn-edit"),
-    btnDeleteElem = document.querySelector(".options-btn-delete"),
+const tableCellsArr = [...document.getElementsByTagName("td")],
+    optionsBtnsArr = [...document.querySelectorAll(".options-btn")],
     modalWindow = document.querySelector(".modal-window"),
+    modalContent = document.querySelector(".modal-content"),
     modalBtn = document.querySelector(".modal-content-btn");
+let inputText = document.querySelector('input[type="text"]');
 
-function selectsTheCell(elem) {
-    console.log(elem.target);
-    elem.target.classList.toggle("active");
+let selectedCell = [];
+
+function checkCellWasSelected() {
+    const pressedCell = document.querySelectorAll(".active");
+    if (pressedCell.length === 0) {
+        alert("Сперва нажмите на ячейку");
+        return false;
+    } else if (pressedCell.length > 1) {
+        Array.from(pressedCell).forEach((elem) => {
+            elem.classList.remove("active");
+        })
+        selectedCell.classList.add("active");
+    }
+    return true;
 }
 
-tableCellsElems.forEach((elem) => {
-    elem.addEventListener("click", (elem) => selectsTheCell(elem));
+function selectsTheCell(elem) {
+    elem.target.classList.toggle("active");
+    selectedCell = elem.target;
+    checkCellWasSelected();
+}
+
+tableCellsArr.forEach((elem) => {
+    elem.addEventListener("click", (elem) => {
+        selectsTheCell(elem);
+    });
 });
 
-btnAddElem.addEventListener("click", () => {
-    modalWindow.style.display = "flex";
-});
+optionsBtnsArr.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        if (checkCellWasSelected()) { 
+            // добавление/редактирование записи
+            if (!Array.from(elem.classList).includes("delete")) {
+                modalWindow.style.display = "flex";
+                inputText.focus();
+                (selectedCell.innerHTML.length > 0) ? inputText.value = selectedCell.innerHTML : inputText.value = "";              
+            } else {
+                // удаление записи
+                (function resetSelectedCell() {
+                    selectedCell.innerHTML = "";
+                }());
+            }
+        }
+    })
+})
 
-btnEditElem.addEventListener("click", () => {
-    modalWindow.style.display = "flex";
-});
+function addValueToCell() {
+    console.log(selectedCell.innerHTML);
+    selectedCell.innerHTML = inputText.value;
+}
 
 modalBtn.addEventListener("click", () => {
+    addValueToCell();
+    inputText.value = "";
     modalWindow.style.display = "none";
 });
 
-
-
-
-
-
-
-
-// function func2() {
-//     console.log("Обработчик 2 работает");
-//     this.removeEventListener("click", func());
-//     this.removeEventListener("dblclick", func2());
-// }
-
-// tableCellsElems.forEach((elem) => {
-//     elem.addEventListener("click", (elem) => selectsTheCell(elem));
-//     elem.addEventListener("dblclick", (elem) => func2(elem));
-// });
+modalWindow.addEventListener("click", (event) => {
+    if (event.target.classList.toString() === "modal-window") {
+        modalWindow.style.display = "none";
+    }
+});
 
