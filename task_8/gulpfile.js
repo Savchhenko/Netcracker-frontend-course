9ecf3012c99230d1d3da51a1ca6c7ff043db2b95
Alexandra Savchenko <savchenko@phys.vsu.ru>
2021-11-21 13:28:54 +0300

    refactoring

--- task_8/gulpfile.js
+++ task_8/gulpfile.js
@@ -3,8 +3,8 @@ const concat = require("gulp-concat");
 const uglify = require("gulp-uglify");
 
 gulp.task("scripts", function() {
-  return gulp.src(["./task_4/**/*.js"])
+  return gulp.src(["../task_4/**/*.js"])
   .pipe(concat("all.js"))
   .pipe(uglify())
-  .pipe(gulp.dest("./task_4/dist"))
+  .pipe(gulp.dest("./dist"))
 })
