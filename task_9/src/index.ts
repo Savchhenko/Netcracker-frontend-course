4ece69260f74034e2086fadc798c9a146b3a2456
Savchenko <svchhh@yandex.ru>
2021-12-23 02:23:16 +0300

    add class Factory and decorator logger

--- task_9/src/index.ts
+++ task_9/src/index.ts
@@ -1,7 +1,8 @@
-// import { Bakery } from "./bakery";
-// import { Cake } from "./cake";
-// import { CakeType } from "./cake-type";
+import { Bakery } from "./bakery";
+import { Cake } from "./cake";
+import { CakeType } from "./cake-type";
 import { CakeInstance } from "./cake-instance";
+import { Factory } from "./factory";
 
 const cake1: CakeInstance = new CakeInstance("Прага", 900, "Тортьяна", 1500, 2000, ["крем", "джем"]);
 console.log(cake1);
@@ -21,6 +22,21 @@ cake1.usePromocode("NY2022");
 cake2.usePromocode("goodday");
 console.log("Цена торта:", cake2.getPrice); //getter
 
-
 console.log(cake1.cakeType); 
 console.log(cake2.cakeType);
+
+//--------------------------------------------------------------
+//using factory:
+const factory: Factory = new Factory;
+
+const bakery1 = factory.create(Bakery, "Тортьяна");
+console.log(bakery1);
+
+const cakeExample = factory.create(Cake, "Тортьяна", 1000, 1800);
+console.log(cakeExample);
+
+const nutsCake = factory.create(CakeType, "Гранат", 1000, 1800, "орехи");
+console.log(nutsCake);
+
+const belgian = factory.create(CakeInstance, "Бельгийский", 800, "Гранат", 1000, 1800, "орехи");
+console.log(belgian);
