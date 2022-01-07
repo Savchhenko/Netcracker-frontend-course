1f014c24eb89fee8fbd2287a71032156d8d1b780
Savchenko <svchhh@yandex.ru>
2022-01-08 02:45:59 +0300

    get response with daily forecast

--- task_11_1/main.js
+++ task_11_1/main.js
@@ -20,6 +20,7 @@ btnElem.addEventListener("click", () => {
             inputElem.value = "";
             showWeather(data);
             updateBtnHandler(document.querySelector(".btn-update"));
+            hourlyForecastBtnHandler(document.querySelector(".btn-hourly"), data);
         })
     .catch(err => console.log("Error: " + err));
 })
@@ -29,13 +30,18 @@ function showWeather(data) {
     const temp = Math.round(data.main.temp - 273.15);
     const imgSrc = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
+    weatherContainer.style.display = "flex";
+
     weatherContainer.innerHTML = `
         <h1 class="city-name large">${data.name}</h1>
         <span>Status at ${time}</span>
         <h2 class="temp">${temp}°C</h2>
         <h2 class="description large">${data.weather[0].description}</h2>
         <img src=${imgSrc}>
-        <button class="btn-update btn">Update</button>
+        <div>
+            <button class="btn-update btn">Update</button>
+            <button class="btn-hourly btn">Hourly forecast</button>
+        <div>
     `;
 };
 
@@ -55,3 +61,51 @@ function updateBtnHandler(btn) {
           };
     });
 };
+
+// Форматирует координату, чтобы она содержала строго 2 цифры после запятой
+calcCoord = function (coord) {
+    if ((coord > 0 && coord > 9) || (coord < 0 && coord > -10)) {
+        coord = coord.toString().slice(0, 5);
+    } else if (coord > 0 && coord < 10) {
+        coord = coord.toString().slice(0, 4);
+    } else if (coord < 0 && coord <= -10) {
+        coord = coord.toString().slice(0, 6);
+    }
+
+    if (coord.split(".")[1].length === 1) {
+        coord = coord + "0";
+    } else if (coord.split(".")[1].length === 0) {
+        coord = coord + "00";
+    }
+
+    return coord;
+};
+
+function hourlyForecastBtnHandler(btn, data) {
+    btn.addEventListener("click", () => {
+        console.log("The third button was pressed!");
+        console.log(data);
+
+        const lon = calcCoord(data.coord.lon);
+        const lat = calcCoord(data.coord.lat);
+        console.log(lon, lat);
+
+        const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,daily&appid=${apiKey}`;
+
+        fetchRequest(api);
+
+        weatherContainer.style.display = "none";
+
+    })
+}
+
+async function fetchRequest(api) {
+    let response = await fetch(api);
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json);
+    } else {
+        alert("Wrong city");
+        console.log("Ошибка HTTP: " + response.status);
+    }
+}
