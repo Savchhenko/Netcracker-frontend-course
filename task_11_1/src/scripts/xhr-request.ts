2ba161cb6176f755bd85ab9287eec8a6b52814ca
Savchenko <svchhh@yandex.ru>
2022-01-08 18:59:08 +0300

    delete comments

--- task_11_1/src/scripts/xhr-request.ts
+++ task_11_1/src/scripts/xhr-request.ts
@@ -2,9 +2,6 @@ import { showWeather } from "./show-weather";
 import { apiKey } from "./main";
 import { currentCityName } from "./main";
 
-// const cityNameElem = <HTMLElement>document.querySelector(".container__weather--city-name");
-// const cityName = cityNameElem.innerHTML;
-
 export function xhrRequest() {
     let xhr = new XMLHttpRequest();
         xhr.open("GET", `http://api.openweathermap.org/data/2.5/weather?q=${currentCityName}&appid=${apiKey}`);
